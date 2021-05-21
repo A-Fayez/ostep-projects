0b82729ca512afbb1bee23b74920e5d8df76d4bc
a-fayez <ahmedf.higgy@gmail.com>
2021-05-21 03:05:09 +0200

    [reverse] stdin correct linked list implementation

--- initial-reverse/reverse.c
+++ initial-reverse/reverse.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+typedef struct node {
+  char *line;
+  struct node *next;
+} node_t;
+
+void push(node_t *head, char *line) {
+  node_t *current = head;
+  while (current->next != NULL) {
+    current = current->next;
+  }
+
+  current->next = (node_t *)malloc(sizeof(node_t));
+  current->next->line = (char *)malloc(strlen(line) + 1);
+  strcpy(current->next->line, line);
+  current->next->next = NULL;
+}
+
+void print_list(node_t *head, FILE *stream) {
+  if (head == NULL)
+    return;
+  print_list(head->next, stream);
+  fprintf(stream, head->line);
+}
 
 int main(int argc, char *argv[]) {
   if (argc > 3) {
@@ -11,14 +36,24 @@ int main(int argc, char *argv[]) {
   char *line = NULL;
   size_t len = 0;
   ssize_t nread;
+  node_t *head = (node_t *)malloc(sizeof(node_t));
   switch (argc) {
     // no args, read from stdin
   case 1:
-    while ((nread = getline(&line, &len, stdin)) != 1) {
-      fprintf(stdout, line);
+    int first = 1;
+    while ((nread = getline(&line, &len, stdin)) != -1) {
+      if (first) {
+        head->line = (char *)malloc(strlen(line) + 1);
+        strcpy(head->line, line);
+        head->next = NULL;
+        first = 0;
+        continue;
+      }
+      push(head, line);
     }
     free(line);
-    exit(0);
     break;
   }
+  print_list(head, stdout);
+  return 0;
 }
